6efcec5d0d039c06a9df96726b8c5c08393aad82
lbchhhh <74388746+lbchhhh@users.noreply.github.com>
2020-11-13 11:51:11 +0800

    类

---
+++ IssueMS/src/main/java/com/ibm/Tables/User.java
@@ -0,0 +1,12 @@
+package com.ibm.Tables;
+
+import java.util.Date;
+
+public class User {
+
+	private int userId;
+	private String userName;
+	private String password;
+	private String email;
+	private Date createDate;
+}
