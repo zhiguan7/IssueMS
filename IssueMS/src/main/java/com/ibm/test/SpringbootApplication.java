eea7da1291a55202f54534ebe66e7940c27cc4f0
0phone <zhiguan7@gmail.com>
2020-11-16 17:42:20 +0800

    更新 SpringbootApplication.java

--- IssueMS/src/main/java/com/ibm/test/SpringbootApplication.java
+++ IssueMS/src/main/java/com/ibm/test/SpringbootApplication.java
@@ -2,10 +2,10 @@ package com.ibm.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAsync
+@EnableJpaRepositories
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
