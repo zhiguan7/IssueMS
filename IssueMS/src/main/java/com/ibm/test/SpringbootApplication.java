eb7a459aabac570a2dc297754e31716226b7af42
0phone <zhiguan7@gmail.com>
2020-11-16 17:24:34 +0800

    springboot框架

---
+++ IssueMS/src/main/java/com/ibm/test/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.ibm.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
